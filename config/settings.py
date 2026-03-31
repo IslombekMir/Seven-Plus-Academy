0a4d7cbc1648936c62422db1d886de8b97637851
Islombek Mirsalimov <mirsalimovislombek9@gmail.com>
2026-04-01 00:47:27 +0500

    add header and csrf trusted origins to settings

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://sevenplusac.uz",
+    "https://www.sevenplusac.uz",
+]
